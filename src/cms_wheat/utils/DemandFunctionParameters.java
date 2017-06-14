b587472d4980a64a890e888580d60150299879fc
Gianfranco Giulioni <g.giulioni@gmail.com>
2017-06-14 14:10:59 +0200

    source code

--- src/cms_wheat/utils/DemandFunctionParameters.java
+++ src/cms_wheat/utils/DemandFunctionParameters.java
@@ -4,14 +4,18 @@ import cms_wheat.Cms_builder;
 
 
 public class DemandFunctionParameters{
-	int intercept;
+	int intercept,slope;
 	String market,producer;
 
 	public DemandFunctionParameters(int theIntercept,String theMarket,String theProducer){
 		intercept=theIntercept;
 		market=theMarket;
 		producer=theProducer;
-		if(Cms_builder.verboseFlag){System.out.println("          new parameters; demand at p=0: "+intercept+", market: "+market+", producer: "+producer);}
+//		if(Cms_builder.verboseFlag){System.out.println("          new parameters; demand at p=0: "+intercept+", market: "+market+", producer: "+producer);}
+	}
+	public void setSlope(double thisDemandFunctionParametersSlope){
+		slope=(int)thisDemandFunctionParametersSlope;
+		if(Cms_builder.verboseFlag){System.out.println("          new parameters; demand at p=0: "+intercept+" at p=10 "+(intercept-slope*10)+", market: "+market+", producer: "+producer);}
 	}
 
 	public void setIntercept(int theNewIntercept){
@@ -21,10 +25,12 @@ public class DemandFunctionParameters{
 	public void increaseInterceptBy(int interceptIncrease){
 		intercept+=interceptIncrease;
 		if(Cms_builder.verboseFlag){System.out.println("                 demand increased by "+interceptIncrease+", market: "+market+", producer: "+producer);}
+//		System.out.println("                 demand increased by "+interceptIncrease+", market: "+market+", producer: "+producer);
 	}
 	public void decreaseInterceptBy(int interceptDecrease){
 		intercept+=-interceptDecrease;
 		if(Cms_builder.verboseFlag){System.out.println("                 demand decreased by "+interceptDecrease+", market: "+market+", producer: "+producer);}
+//		System.out.println("                 demand decreased by "+interceptDecrease+", market: "+market+", producer: "+producer);
 	}
 
 	public int getIntercept(){
