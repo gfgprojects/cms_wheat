1dfc2517066927b4f30d8d09a4749691f6033f80
Gianfranco Giulioni <g.giulioni@gmail.com>
2018-04-26 10:26:45 +0200

    check scripts folder

---
+++ scripts/checks/r_check_supply_and_demand_balance_after_producers_reduction.R
@@ -0,0 +1,21 @@
+#check at repast input folder
+
+production<-read.csv("../../data/producers.csv")
+total_prod<-colSums(production[,8:ncol(production)])
+
+food<-read.csv("../../data/buyers_Food.csv",)
+feed<-read.csv("../../data/buyers_Feed.csv",)
+seed<-read.csv("../../data/buyers_Seed.csv",)
+misc<-read.csv("../../data/buyers_Misc.csv",)
+
+demand<-food[,5:ncol(food)]+feed[,5:ncol(feed)]+seed[,5:ncol(seed)]+misc[,5:ncol(misc)]
+total_dem<-colSums(demand)
+
+first.year.of.demand<-as.numeric(unlist(strsplit(names(food)[5],"[.]"))[3])
+last.year.of.demand<-as.numeric(unlist(strsplit(names(food)[ncol(food)],"[.]"))[3])
+first.year.of.production<-first.year.of.demand-1
+last.year.of.production<-last.year.of.demand
+
+plot(seq(first.year.of.production,first.year.of.production-1+length(total_prod)),total_prod,type="l",main="production (black) and demand (red)",xlab="time",ylab="quantity")
+lines(seq(first.year.of.demand,first.year.of.production-1+length(total_prod)),total_dem,col=2)
+
